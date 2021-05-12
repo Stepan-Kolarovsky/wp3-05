72796161835e2ceaf3a57ad33a33034bd8040280
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2021-05-12 11:42:39 +0200

    XX

--- classes/Computer.php
+++ classes/Computer.php
@@ -2,14 +2,51 @@
  class Computer
 {   
     private $name;
+    private $cpu;
+    private $gpu;
+    private $ssd;
+    private $ram;
+    private $mb;
+    private $psu;
     
-    public function __construct(string $name) {
+    public function __construct(string $name, string $cpu, string $gpu,string $ssd, string $ram, string $mb, string $psu) {
         $this ->name = $name;
+        $this ->cpu = $cpu;
+        $this ->gpu = $gpu;
+        $this ->ssd = $ssd;
+        $this ->ram = $ram;
+        $this ->mb = $mb;
+        $this ->psu = $psu;
 }
 
 public function summary() {
-    echo "Počítač s názvem <strong>$this->name</strong> obsahuje skvělé komponenty";
+    echo "název PC: $this->name<br>cpu:$this->cpu<br>gpu:$this->gpu<br>ssd:$this->ssd<br>ram:$this->ram<br>mb:$this->mb<br>psu:$this->psu";
+    echo "<br>";
 
 }
+public function getname() {
+    return $this->name;
+}
+public function getcpu() {
+    return $this->cpu;
+}
+
+public function getgpu() {
+    return $this->gpu;
+}
+public function getssd() {
+    return $this->ssd;
+}
+public function getram() {
+    return $this->ram;
+}
+public function getmb() {
+    return $this->mb;
+}
+public function getpsu() {
+    return $this->psu;
+}
+
+
 }
 ?>
--- index.php
+++ index.php
@@ -10,11 +10,40 @@
 </head>
 <body>
     <?php
-     $computer1 = new Computer("pepíkuv počítač");
-     $computer2 = new Computer("kájův počítač");
-     $computer1 ->summary();?> <br />
-     <?php
-     $computer2 ->summary();
-    ?> 
+     $computers[] = new Computer(" Pepíkuv počítač"," Intel Core i7 10900K"," Nvidia RTX 3070 16GB"," 256GB"," 16GB"," Asus"," Corsair 750W");
+     $computers[] = new Computer(" Kájův počítač", " Amd Ryzen 9 5900X"," AMD Radeon RX 6700X"," 500GB"," 32GB"," Gigabyte"," Be quiet 850W");
+     $computers[] = new Computer(" Lakyho počítač", " Amd Ryzen 7 3700X"," Nvidia GeForce 1660 6GB"," 1TB"," 16GB"," Gigabyte"," Corsair 650W");
+     $computers[] = new Computer(" Franty počítač", " Amd Ryzen 5 3600X"," Nvidia RTX 2080 8GB"," 2TB"," 32GB"," Asus"," Asus 800W");
+     $computers[] = new Computer(" Honzy počítač", " Intel Core i9 10900k"," Nvidia RTX 3090 16GB"," 5TB"," 64GB"," Asus"," Corsair 1200W");
+     ?>
+    
+<h1>Výpis Názvů</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getname() . "<br>";}
+?>
+<h1>Výpis CPU</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getcpu() . "<br>";}
+?>
+<h1>Výpis GPU</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getgpu() . "<br>";}
+?>
+<h1>Výpis SSD</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getssd() . "<br>";}
+?>
+<h1>Výpis RAM</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getram() . "<br>";}
+?>
+<h1>Výpis MB</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getmb() . "<br>";}
+?>
+<h1>Výpis PSU</h1>
+    <?php
+    foreach ($computers as $computer){echo $computer->getpsu() . "<br>";}
+?>
 </body>
 </html>
